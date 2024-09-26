c756e3777a741321dc908cf70e50855cfcb5d1b9
unknown <79578683+Treszyk@users.noreply.github.com>
2024-09-26 18:54:11 +0200

    Init

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter
+
+if __name__=='__main__':
+    app = tkinter.Tk()
+    app.iconbitmap("Youtube_logo.ico")
+    app.title('Youtube Video Downloader')
+    #app.config(bg="#26242f")   
+    app.mainloop()
