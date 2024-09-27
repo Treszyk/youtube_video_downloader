c87a14f2a2aedd0c4739187b3170d3a2d2d60b30
unknown <79578683+Treszyk@users.noreply.github.com>
2024-09-28 00:31:06 +0200

    Added download functionality, added history of downloaded videos

--- input_box.py
+++ input_box.py
@@ -3,20 +3,20 @@ import tkinter as tk
 class InputBox:
     def __init__(self, root):
         self.root = root
-        self.entry = tk.Entry(root, width=40, font=("Calibri 12"))
-        self.entry.insert(0, 'enter video URL')
-        self.entry.config(fg='grey')
-        self.entry.bind('<FocusIn>', self.on_entry_click)
-        self.entry.bind('<FocusOut>', self.on_focusout)
-        self.entry.pack(pady=10, ipady=10)
+        self.entry_box = tk.Entry(root, width=40, font=("Calibri 12"))
+        self.entry_box.insert(0, 'enter video URL')
+        self.entry_box.config(fg='grey')
+        self.entry_box.bind('<FocusIn>', self.on_entry_box_click)
+        self.entry_box.bind('<FocusOut>', self.on_focusout)
+        self.entry_box.pack(pady=10, ipady=10)
     
-    def on_entry_click(self, e):
-        if self.entry.get() == 'enter video URL':
-            self.entry.delete(0, "end")  # delete all the text in the entry
-            self.entry.insert(0, '')  # insert blank for user input
-            self.entry.config(fg='black')
+    def on_entry_box_click(self, e):
+        if self.entry_box.get() == 'enter video URL':
+            self.entry_box.delete(0, "end")  # delete all the text in the entry_box
+            self.entry_box.insert(0, '')  # insert blank for user input
+            self.entry_box.config(fg='black')
 
     def on_focusout(self, e):
-        if self.entry.get() == '':
-            self.entry.insert(0, 'enter video URL')
-            self.entry.config(fg='grey')
+        if self.entry_box.get() == '':
+            self.entry_box.insert(0, 'enter video URL')
+            self.entry_box.config(fg='grey')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from input_box import InputBox
 from PIL import Image, ImageTk
+from tab_switcher import TabSwitcher
 
 def load_image(image_path, root):
     if image_path:
@@ -12,27 +13,19 @@ def load_image(image_path, root):
         img_label = tk.Label(root, image=img_tk)
         img_label.pack(pady=10)
 
-def download_video():
-    pass
+
 if __name__=='__main__':
     app = tk.Tk()
     app.resizable(0,0)
     app.iconbitmap("Youtube_logo.ico")
     app.title('Youtube Video Downloader')
-    #app.config(bg="#26242f")   
     app.geometry("400x400")
-    app.minsize(450, 350)
-    app.maxsize(450, 350)
-    image_path = 'Youtube_logo.png'
-    if image_path:
-        img = Image.open(image_path)
-        img = img.resize((250, 173))  # Resize image to fit
-        img_tk = ImageTk.PhotoImage(img)
+    app.minsize(450, 400)
+    app.maxsize(450, 400)
+    #app.config(bg="#26242f")   
 
-        # Create a label for the image and place it at the top
-        img_label = tk.Label(app, image=img_tk)
-        img_label.pack(pady=10)
-    entry = InputBox(app)
-    download_button = tk.Button(app, text="DOWNLOAD", command=download_video, width=40, font=("Calibri 12"))
-    download_button.pack(pady=10, ipady=10)
+    switcher = TabSwitcher(app)
+
+
+
     app.mainloop()
